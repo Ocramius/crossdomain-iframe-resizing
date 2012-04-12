cb66855e34613dee461bdc6f758582c64872341a
Marco Pivetta <ocramius@gmail.com>
2012-04-12 16:35:10 +0200

    Making frame resizing behavior persistent over multiple page refreshes (still has to be differentiated by frame id)

--- js/HeightPublisher.js
+++ js/HeightPublisher.js
@@ -1,56 +1,69 @@
-(function(exports, $) {
+(function (exports, $) {
 
-    var HeightPublisher = (function() {
+    var HeightPublisher = (function () {
 
-        var lastLocation = '';
         var currentFrameId = null;
-        
-        this.publishHeight = function() {
-            if (window.location.hash.length == 0) return;
+        var cookieName = 'HeightPublisher';
+        var cookieOptions = {
+            expires:7,
+            path:'/'
+        };
+
+        this.publish = function (params) {
+            var hashParams = this.read();
+            if (!hashParams['lastLocation']) {
+                return false;
+            }
+            hashParams = $.extend({}, hashParams, params);
+            var hashString = EncoderTools.buildHashParams(hashParams);
+            $.cookie(cookieName, hashString, cookieOptions);
+            window.parent.location = hashParams['lastLocation'] + '#' + hashString;
+            console.log(hashParams)
+        };
+
+        this.read = function () {
+            var hashParams = EncoderTools.parseHashParams(window.location.hash.substring(1));
+            if (!hashParams['lastLocation']) {
+                hashParams = EncoderTools.parseHashParams($.cookie(cookieName) || "");
+            }
+            return hashParams;
+        };
+
+        this.publishHeight = function () {
             var frameId = getFrameId();
-            if (frameId == '') {
+            if (!frameId) {
                 return;
             }
             var actualHeight = getBodyHeight();
             var currentHeight = getViewPortHeight();
-            if  (Math.abs(actualHeight/currentHeight - 1) > 0.05) {
-                var params = EncoderTools.parseHashParams(window.location.hash.substring(1));
-                if(
-                    typeof params['lastLocation'] != 'undefined'
-                    && params['lastLocation']
-                ) {
-                    lastLocation = params['lastLocation'];
-                }
-                if (lastLocation != '') {
-                    params['lastLocation'] = lastLocation;
-                    params['frameId'] = frameId;
-                    params['height'] = actualHeight.toString();
-                    window.parent.location = lastLocation + '#' + EncoderTools.buildHashParams(params);
-                }
+            if (Math.abs(actualHeight / currentHeight - 1) > 0.05) {
+                this.publish({
+                    height:actualHeight.toString()
+                });
             }
         };
 
-        var getFrameId = function() {
-            var qs = EncoderTools.parseHashParams(window.location.hash.substring(1));
+        var getFrameId = function () {
+            var qs = this.read();
             var frameId = qs['frameId'];
-            if(frameId) {
+            if (frameId) {
                 currentFrameId = frameId;
             }
             return currentFrameId;
         };
 
-        var getBodyHeight = function() {
+        var getBodyHeight = function () {
             return $(document.body).outerHeight();
         };
 
-        var getViewPortHeight = function() {
+        var getViewPortHeight = function () {
             var height = 0;
             if (window.innerHeight) {
                 height = window.innerHeight - 18;
             } else if (
                 document.documentElement
-                && document.documentElement.clientHeight
-            ) {
+                    && document.documentElement.clientHeight
+                ) {
                 height = document.documentElement.clientHeight;
             } else if (document.body && document.body.clientHeight) {
                 height = document.body.clientHeight;
@@ -62,8 +75,8 @@
 
     })();
 
-    $(function(){
-        var publishHeight = function() {
+    $(function () {
+        var publishHeight = function () {
             HeightPublisher.publishHeight();
             window.setTimeout(publishHeight, 500);
         }
